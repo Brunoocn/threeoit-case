862e6181e524852e743cab740947872558faa9ec
brunoocn <bruno.caneo@contemplato.com>
2024-01-20 17:13:32 -0300

    fix: ajustando bug de cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS
+    ? process.env.CORS_ALLOWED_ORIGINS.split(',')
+    : '';
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({
+    origin: allowedOrigins,
+  });
+
   await app.listen(3005);
 }
 bootstrap();
